395d605bae4871b1e082e2ed6085cfd33414fb87
AugustoPaiva <augustopaiva4@gmail.com>
2017-07-24 14:17:42 -0300

    Adicionando sessions

--- app/src/main/java/comviewzonazul/google/httpssites/zonazul/infraestrutura/DatabaseHelper.java
+++ app/src/main/java/comviewzonazul/google/httpssites/zonazul/infraestrutura/DatabaseHelper.java
@@ -18,7 +18,9 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         //Tabela de usuarios
         db.execSQL("create table Usuarios(_id integer primary key autoincrement, "
                 +"nome text not null, login text not null, senha text not null)");
-
+         // Tabela de clientes
+        db.execSQL("create table Clientes(_id integer primary key autoincrement, "
+                +"email text not null, cep text not null, complemento text not null, numero text not null, cidade text not null, id_usuario integer)");
         //Cadastrar um usuario
         db.execSQL("insert into Usuarios(nome, login, senha) values('Admin', 'admin','123')");
     }
@@ -36,6 +38,21 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
         public static final String[] COLUNAS = new String[]{
                 _ID, NOME, LOGIN, SENHA
+        };
+    }
+    public static class Clientes{
+        public static final String TABELA_CLIENTES = "clientes";
+        public static final String _ID = "_id";
+        public static final String EMAIL = "email";
+        public static final String CEP = "cep";
+        public static final String COMPLEMENTO = "complemento";
+        public static final String NUMERO = "numero";
+        public static final String CIDADE = "cidade";
+        public static final String ID_USUARIO = "id_usuario";
+
+
+        public static final String[] COLUNAS_CLIENTES = new String[]{
+                _ID, EMAIL, CEP, COMPLEMENTO, NUMERO, CIDADE, ID_USUARIO
         };
     }
 
